e24ce3e456474b06a86108926387637cb77a9eec
Cameron Williams <cswilliams112004@gmail.com>
2024-01-17 15:10:37 -0500

    reversed_list.py

---
+++ reversed_list.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 99, 2600, 300]
+
+#reversing the list here using the reversed() function
+reversed_list = list(reversed(numbers))
+print("Original List:", numbers)
+print("Reversed list:", reversed_list)
